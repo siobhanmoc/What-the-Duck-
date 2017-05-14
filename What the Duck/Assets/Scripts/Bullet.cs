8591baae470f81364fb5ff568fb65caa65d55c32
siobhanmoc <siobhanmoc@gmail.com>
2017-05-15 08:49:20 +1000

    first commit

---
+++ What the Duck/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public int damage = 20;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 4f);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Enemy") {
+			other.GetComponent<PlayerController>() .TakeDamage (damage);
+		}
+
+		Destroy (this.gameObject);
+	}
+}
---
+++ What the Duck/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float bulletspeed = 100;
+
+	public GameObject bulletPrefab;
+	public GameObject gun;
+
+	public Transform bulletSpawnPoint;
+
+	public int health = 100;
+	public void TakeDamage(int damageToTake) {
+		health = health - damageToTake;
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+
+		if (health <= 0) {
+			//this exits the function and does not run anything after it.
+			return;
+		}
+
+		transform.position = transform.position + new Vector3 (0.1f, 0, 0);
+		if (Input.GetKeyUp (KeyCode.A)) {
+			transform.position = transform.position + new Vector3 (0, 0, 1);
+		}
+		if (Input.GetKeyUp (KeyCode.D)) {
+			transform.position = transform.position + new Vector3 (0, 0, -1);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = transform.position + new Vector3 (0, 0.4f, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			transform.position = transform.position + new Vector3 (0, 0, 1);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			transform.position = transform.position + new Vector3 (0, 0, -1);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position = transform.position + new Vector3 (0, 0.4f, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject GO = Instantiate (bulletPrefab, bulletSpawnPoint.position, Quaternion.identity) as GameObject;
+			GO.GetComponent<Rigidbody> ().AddForce (gun.transform.forward * bulletspeed, ForceMode.Impulse);
+		}
+	}
+}
